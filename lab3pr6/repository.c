7c3cc98e3f183baf916931f498a312d64b8ea368
Diac Eugen Codrin <codrin1999@yahoo.com>
2019-03-14 18:23:43 +0200

    Update repository.c

--- lab3pr6/repository.c
+++ lab3pr6/repository.c
@@ -7,7 +7,7 @@
 #include "domain.h"
 
 
-void repositoryIniSetUp(world **currentWorld)
+void repositoryInitSetUp(world **currentWorld)
 {
 	*currentWorld = malloc(sizeof(world));
 	(*currentWorld) -> lenght = 0;
@@ -17,18 +17,18 @@ void repositoryGetAllCountries(world *currentWorld)
 {
 	for (int i = 0; i < currentWorld -> lenght; ++i)
 	{
-		printf("\tname - %d\n",currentWorld -> name);
-		printf("\tcontinent - %d\n",currentWorld -> continent);
-		printf("\tpopulation - %d\n",currentWorld -> population);
+		printf("\tname - %s\n",currentWorld -> con[i].name);
+		printf("\tcontinent - %s\n",currentWorld -> con[i].continent);
+		printf("\tpopulation - %lli\n",currentWorld -> con[i].population);
 	}
 }
 
-void repositoryAddCountry(world *currentWorld,char name[50],char continent[50],long long population)
+void repositoryAddCountry(world *currentWorld,char name[],char continent[],long long population)
 {
 	int index = -1;
 	for (int i = 0; i < currentWorld -> lenght; ++i)
 	{
-		if (currentWorld ->con[i].name == name)
+		if (strcmp(currentWorld -> con[i].name, name) == 0)
 		{
 			index = i;
 			break;
@@ -37,7 +37,7 @@ void repositoryAddCountry(world *currentWorld,char name[50],char continent[50],l
 
 	if (index == -1)
 	{
-		prinf("We are adding a new country to the repository");
+		printf("We are adding a new country to the repository...\n");
 		Country newCountry;
 		newCountry = create_country(name,continent,population);
 		currentWorld -> con[currentWorld -> lenght++] = newCountry;
@@ -69,12 +69,12 @@ void repositoryUpdateCountry(world *currentWorld,int index,char name[],char cont
 {
 	if (index < 0 || index > currentWorld -> lenght)
 	{
-		printf("Index out of rande...")
+		printf("Index out of rande...");
 	}
 	else
 	{
-		currentWorld -> con[index].name = name;
-		currentWorld -> con[index].continent = continent;
+		strcpy(currentWorld -> con[index].name,name);
+		strcpy(currentWorld -> con[index].continent,continent);
 		currentWorld -> con[index].population = population;
 	}
 }
@@ -87,11 +87,47 @@ void repositoryMigration(world *currentWorld,int index1,int index2,long long nr)
 	}
 	else
 	{
-		printf("A part of the population of the country from index1 migrates towards the country from index2...")
-		long long a = currentWorld -> con[index1].population - nr;
-		currentWorld -> con[index1].population = a;
-		long long a = currentWorld -> con[index2].population + nr;
-		currentWorld -> con[index2].population = a;
+		printf("A part of the population of the country from index1 migrates towards the country from index2...");
+		currentWorld -> con[index1].population -= nr;
+		currentWorld -> con[index2].population += nr;
+	}
+}
+
+int containsString(char *st1,char *st2)
+{
+	char *res;
+	void *ret;
+	ret = NULL;
+	res = strstr(st1,st2);
+	if (res == ret) return 0;
+	return 1;
+}
+
+void repositoryGetAllWhoContain(world *currentWorld,char substring[])
+{
+
+	if (substring[0] == '\0')
+	{
+		repositoryGetAllCountries(currentWorld);
+	}
+	else
+	{
+		Country countries[100];
+		int index = 0;
+		for (int i = 0; i < currentWorld -> lenght; ++i)
+		{
+			if (containsString(currentWorld -> con[i].name, substring) == 1)
+			{
+				countries[index++] = currentWorld -> con[i];
+			}
+		}
+
+		for (int i = 0; i < index; ++i)
+		{
+			printf("\tname - %s\n",countries[i].name);
+			printf("\tcontinent - %s\n",countries[i].continent);
+			printf("\tpopulation - %lli\n",countries[i].population);
+		}
 	}
 }
 
